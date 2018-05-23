3b3191d1904f770e26759860fde58f291896bbde
kimdongwon <ehddnjs8989@gmail.com>
2018-05-23 15:06:34 +0900

    로그인 토큰 response 받음, 새로고침 전

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import axios from 'axios';
 
+const postAPI = axios.create({
+
+});
+
 const rootEl = document.querySelector(".root");
 const templates = {
   postList: document.querySelector("#post-list").content,
   postItem: document.querySelector("#post-item").content,
-  postContent: document.querySelector("#post-content").content
+  postContent: document.querySelector("#post-content").content,
+  login: document.querySelector('#login').content
 };
 
 function render(fragment) {
@@ -13,8 +18,11 @@ function render(fragment) {
 }
 
 async function indexPage() {
-  const res = await axios.get('http://localhost:3000/posts');
+  const res = await postAPI.get('http://localhost:3000/posts');
   const listFragment = document.importNode(templates.postList, true);
+  listFragment.querySelector('.post-list__login-btn').addEventListener('click', e=>{
+    loginPage();
+  })
   res.data.forEach(post => {
     const fragment = document.importNode(templates.postItem, true);
     const pEl = fragment.querySelector('.post-item__title');
@@ -29,7 +37,7 @@ async function indexPage() {
 }
 
 async function postContentPage(postId) {
-  const res = await axios.get(`http://localhost:3000/posts/${postId}`)
+  const res = await postAPI.get(`http://localhost:3000/posts/${postId}`)
   const fragment = document.importNode(templates.postContent,true)
   fragment.querySelector('.post-content__title').textContent = res.data.title;
   fragment.querySelector(".post-content__body").textContent = res.data.body;
@@ -39,4 +47,20 @@ async function postContentPage(postId) {
  render(fragment)
 }
 
+async function loginPage(){
+  const fragment = document.importNode(templates.login, true);
+  const formEl = fragment.querySelector('.login__form');
+  formEl.addEventListener('submit', async e => {
+    const payload = { // 싣어서 보낼 객체 생성
+      username: e.target.elements.username.value, // username: e.target.elements.username === fragment.querySelector('.login__username'); // target은 이벤트가 일어난 객체 e.target에는 form이 들어있음
+      password: e.target.elements.password.value 
+    }; 
+     e.preventDefault();
+     const res = await postAPI.post('http://localhost:3000/users/login', payload);
+      localStorage.setItem('token', res.data.token);
+      postAPI.defaults.headers['Authorization'] = res.data.token;
+      indexPage();
+  })
+  render(fragment);
+}
 indexPage();
